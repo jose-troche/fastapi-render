4b2b8ea69a48dbaf1b90080f3e188a86ebb4d1ae
Jose Troche <jose.troche.coder@gmail.com>
2025-03-22 13:15:17 -0400

    Update main.py with a new route

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World from Fast API"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/items/")
+def read_items():
+    return {"items": [{"id": i} for i in range(5)]}
